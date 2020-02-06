d4e062c4790a741925c05e30dc1bc9b9f5ff9947
Alexander Mikhaylov <alex01236@gmail.com>
2020-02-07 01:34:50 +0300

    UI complete(minus UI logic)

--- actor.js
+++ actor.js
@@ -1,28 +1,15 @@
-let slides = document.querySelector('.pics-slide').children;
-let prevBtn = document.querySelector('.prev-btn');
-let nextBtn = document.querySelector('.next-btn');
-let index = 0;
 
-nextBtn.onclick = () => {
-    switchSlide('next');
-}
 
-prevBtn.onclick = () => {
-    switchSlide('prev');
-}
-
-function switchSlide(direction) {
-    slides[index].classList.remove("active");
-    if (direction == 'next') {
-        index++;
-        if (index >= slides.length) {
-            index = 0;
-        }
-    } else {
-        index--;
-        if (index < 0) {
-            index = slides.length - 1;
-        }
-    }
-    slides[index].classList.add("active");
-}
+$(document).ready(function(){
+    $('.glider').slick({
+        autoPlay: true,
+        dots: true,
+        infinite: true,
+        swipeToSlide: true,
+        variableWidth: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "100px"
+    });
+});
--- indexParallax.js
+++ indexParallax.js
@@ -2,8 +2,4 @@ const parallax = document.getElementById('parallax');
 window.addEventListener("scroll", function () {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = offset*0.7+"px";
-})
-
-const actressesRef = document.getElementById("actressesRef");
-const actorsRef = document.getElementById("actorsRef");
-
+});
